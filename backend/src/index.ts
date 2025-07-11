871e61e3e11c66518f5da021890e23f45a38785c
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-07-11 15:01:37 +0000

    add days difference api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,21 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+app.use(express.json());
 const PORT = 5000;
 
 app.get('/api/message', (_req, res) => {
   res.send('Welcome to Event Desk');
 });
 
+app.post('/api/days', (req: Request, res: Response) => {
+  const { from, to } = req.body;
+
+  const remanigTimes = new Date(to).getTime() - new Date(from).getTime();
+
+  res.status(201).json({ days: remanigTimes });
+});
+
 app.listen(PORT, () => {
   console.log(`Backend running at http://localhost:${PORT}`);
 });
-
